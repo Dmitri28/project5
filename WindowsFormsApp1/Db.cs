da543c98589b9a20aa6288c71726bc19a9e82ed2
Dima <117075948+Dmitri28@users.noreply.github.com>
2022-10-31 12:33:36 +0200

    Project5
    Project5

---
+++ WindowsFormsApp1/Db.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    class Db
+    {
+        MySqlConnection conection=new MySqlConnection("server=localhost;port=3306;username=root;password=root;database=people");
+      public void open()
+        {
+            if (conection.State == System.Data.ConnectionState.Closed)
+                conection.Open();
+        }
+        public void close()
+        {
+            if (conection.State == System.Data.ConnectionState.Open)
+                conection.Close();
+        }
+        public MySqlConnection GetConnection()
+        {
+           return conection;
+        }
+
+        
+
+        }
+    }
+
